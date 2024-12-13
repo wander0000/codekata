85556c236e5b60459e025a4c67c8cac33fe48369
Yeonju Seo <yeonjuseo712@gmail.com>
2024-12-13 10:00:33 +0900

    2024.12.13 3총사(3중 for문)

---
+++ src/main/java/com/sparta/codekata/year2024/month12/ThreeMusketeers.java
@@ -0,0 +1,17 @@
+package com.sparta.codekata.year2024.month12;
+
+public class ThreeMusketeers {
+	public int solution(int[] number) {
+		int cnt = 0;
+		for (int i = 0; i < number.length - 2; i++) {
+			for (int j = i + 1; j < number.length - 1; j++) {
+				for (int k = j + 1; k < number.length; k++) {
+					if (number[i] + number[j] + number[k] == 0) {
+						cnt++;
+					}
+				}
+			}
+		}
+		return cnt;
+	}
+}
